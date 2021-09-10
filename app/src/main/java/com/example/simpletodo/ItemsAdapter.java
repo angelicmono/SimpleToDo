0474ccc77fdd98ae5d33897320b5ab2fd9872bc5
angelicmono <82055538+angelicmono@users.noreply.github.com>
2021-09-09 19:25:39 -0700

    Update

--- app/src/main/java/com/example/simpletodo/ItemsAdapter.java
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -13,16 +13,25 @@ import java.util.List;
 
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
 
-    List<String> items;
-
     public ItemsAdapter(List<String> items) {
+
+    }
+
+    public interface OnLongClickListener {
+        void onItemLongClicked(int position);
+    }
+
+    List<String> items;
+    OnLongClickListener longClickListener;
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener) {
         this.items = items;
+        this.longClickListener = longClickListener;
 
     }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // Use layout inflator to inflate a view
+        // Use layout inflater to inflate a view
 
         View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
         //wrap it inside a View Holder and return it
@@ -56,6 +65,14 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
         //Update the view inside the view holder with this data
         public void bind(String item) {
             tvItems.setText(item);
+            tvItems.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    //Remove the item from the recycler view
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+            });
 
         }
     }
